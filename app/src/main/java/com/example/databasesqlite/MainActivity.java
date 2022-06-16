c08a4a40eacf6630d602e73339758cdc8305f1a8
Pong keosithong <pongkst22@gmail.com>
2022-06-16 16:05:13 +0700

    new design1

--- app/src/main/java/com/example/databasesqlite/MainActivity.java
+++ app/src/main/java/com/example/databasesqlite/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("SQLite");
         save = findViewById(R.id.save);
         show  = findViewById(R.id.show);
 
